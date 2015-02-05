64a115242a04c0038010e41108f3d94d42349265
jasonyoung <jas.young314@gmail.com>
2015-02-05 15:12:31 +0200

    added recursive factorial

--- factorial.py
+++ factorial.py
@@ -3,3 +3,10 @@ def iter_factorial(n):
     for i in range(2, n + 1):
         r *= i
     return r
+
+def recur_factorial(n):
+    
+    if n == 1:
+        return 1
+
+    return n * recur_factorial(n - 1)
